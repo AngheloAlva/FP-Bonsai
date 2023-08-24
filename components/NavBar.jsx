531f9fb9a9617385ad34ac6ad95343c160f3cb7a
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-23 21:09:25 -0400

    install and use react-icons

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import '../styles/navbar.css'
+import { FaBars } from 'react-icons/fa6'
 
 const NavBar = () => {
   return (
     <nav>
       <img src='../public/bonsai-logo.svg' alt='Bonsai Logo' />
 
-      <ul>
+      <FaBars />
+      {/* <ul>
         <div>
           <li><a href='#'>Product</a></li>
           <li><a href='#'>Templates</a></li>
@@ -18,7 +20,7 @@ const NavBar = () => {
           <li><button>LOG IN</button></li>
           <li><button>START FREE</button></li>
         </div>
-      </ul>
+      </ul> */}
     </nav>
   )
 }
