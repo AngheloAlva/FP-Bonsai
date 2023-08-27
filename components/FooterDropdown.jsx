604e3a5b4ade15ccf18c19f113821dedd846de4d
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-27 14:56:23 -0400

    add animation for footer dropdowns

--- components/FooterDropdown.jsx
+++ components/FooterDropdown.jsx
@@ -1,28 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import { FaAngleDown } from 'react-icons/fa6'
+// import { useSpring, animated } from '@react-spring/web'
 
 const FooterDropdown = ({ textLink, dropdownLinks = [] }) => {
   const [dropdownActive, setDropdownActive] = useState(false)
 
   return (
-    <li>
+    <li className='footer-dropdown-container'>
       <div>
         <a href='#'>{textLink}</a>
-        <FaAngleDown style={{ marginTop: '.2rem' }} onClick={() => setDropdownActive(!dropdownActive)} />
+        <FaAngleDown style={{ marginTop: '.15rem' }} onClick={() => setDropdownActive(!dropdownActive)} />
       </div>
       {
-        dropdownActive && (
-          <ul className='footer-dropdown'>
-            {
-              dropdownLinks.map((link, index) => (
-                <li key={index}>
-                  <a href='#'>{link}</a>
-                </li>
-              ))
-            }
-          </ul>
-        )
+        <ul className={`footer-dropdown ${dropdownActive ? 'footer-dropdown-active' : ''}`} >
+          {
+            dropdownLinks.map((link, index) => (
+              <li key={index}>
+                <a href='#'>{link}</a>
+              </li>
+            ))
+          }
+        </ul>
       }
     </li>
   )
