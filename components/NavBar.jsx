40ba7a88c8d8d4ee0a69d77345b2f163eb703cd8
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-28 19:05:50 -0400

    add new style for navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,29 +1,42 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars } from 'react-icons/fa6'
 import '../styles/navbar.css'
 
 const NavBar = () => {
-  return (
-    <nav>
-      <a href='#'>
-        <img src='../public/bonsai-logo.svg' alt='Bonsai Logo' />
-      </a>
+  const [navScroll, setNavScroll] = useState(false)
 
-      <FaBars className='navbar__menu-icon' />
-      {/* <ul>
-        <div>
-          <li><a href='#'>Product</a></li>
-          <li><a href='#'>Templates</a></li>
-          <li><a href='#'>Pricing</a></li>
-          <li><a href='#'>Reviews</a></li>
-        </div>
-        <div>
-          <li><button>LOG IN</button></li>
-          <li><button>START FREE</button></li>
-        </div>
-      </ul> */}
-    </nav>
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      setNavScroll(true)
+    } else {
+      setNavScroll(false)
+    }
+  })
+
+  return (
+    <>
+      <div className={navScroll ? 'nav-scroll-active' : ''} />
+      <nav>
+        <a href='#'>
+          <img src='../public/bonsai-logo.svg' alt='Bonsai Logo' />
+        </a>
+
+        <FaBars className='navbar__menu-icon' />
+        {/* <ul>
+          <div>
+            <li><a href='#'>Product</a></li>
+            <li><a href='#'>Templates</a></li>
+            <li><a href='#'>Pricing</a></li>
+            <li><a href='#'>Reviews</a></li>
+          </div>
+          <div>
+            <li><button>LOG IN</button></li>
+            <li><button>START FREE</button></li>
+          </div>
+        </ul> */}
+      </nav>
+    </>
   )
 }
 
