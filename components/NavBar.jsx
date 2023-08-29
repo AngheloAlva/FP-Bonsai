b8638c6745c2ddde1b3d8b84ab23a250066a9b8e
Anghelo Alva <ance.anghelo@gmail.com>
2023-08-29 11:47:15 -0400

    add nav menu without animation

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import { FaBars } from 'react-icons/fa6'
+import { FaBars, FaXmark, FaAngleDown } from 'react-icons/fa6'
 import '../styles/navbar.css'
+import StartFreeButon from './StartFreeButon'
 
 const NavBar = () => {
   const [navScroll, setNavScroll] = useState(false)
+  const [navMenu, setNavMenu] = useState(false)
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 100) {
@@ -22,7 +24,30 @@ const NavBar = () => {
           <img src='../public/bonsai-logo.svg' alt='Bonsai Logo' />
         </a>
 
-        <FaBars className='navbar__menu-icon' />
+        <FaBars className='navbar__menu-icon' onClick={() => setNavMenu(true)} />
+
+        {
+          navMenu && (
+            <div className={`navbar-menu ${navMenu ? 'navbar-menu-active' : ''}`}>
+              <div className='menu-logo'>
+                <img src='../public/bonsai-logo.svg' alt='Bonsai Logo' />
+                <FaXmark className='navbar__menu-icon' onClick={() => setNavMenu(false)} />
+              </div>
+
+              <div className='menu-options'>
+                <div>Product <FaAngleDown /></div>
+                <div>Templates <FaAngleDown /></div>
+                <a href='#'>Pricing</a>
+                <a href='#'>Reviews</a>
+              </div>
+
+              <div className='menu-btn-container'>
+                <button className='btn-login'>LOG IN</button>
+                <StartFreeButon />
+              </div>
+            </div>
+          )
+        }
         {/* <ul>
           <div>
             <li><a href='#'>Product</a></li>
